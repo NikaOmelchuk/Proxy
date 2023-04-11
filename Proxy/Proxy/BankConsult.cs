095b547a6305656620a0c919980449f80e4b3b0b
NikaOmelchuk <120196531+NikaOmelchuk@users.noreply.github.com>
2023-04-11 16:45:06 +0300

    Add files via upload

---
+++ Proxy/Proxy/BankConsult.cs
@@ -0,0 +1,53 @@
+﻿
+namespace Proxy
+{
+    public interface BankConsult
+    {
+        string GetCons(int q);
+    }
+
+    public class BankCons : BankConsult
+    {
+        public string GetCons(int q)
+        {
+            switch (q)
+            {
+                case 1:
+                    return "Необхiдно мати iдентифiкацiйний документ та поштову адресу.";
+
+                case 2:
+                    return "Ви можете поповнити рахунок через банкомат, iнтернет-банкiнг або шляхом переказу з iншого рахунку.";
+
+                case 3:
+                    return "Мiнiмальна сума для зняття залежить вiд банку та типу картки. Зазвичай це 100 грн або еквiвалентна сума в iншiй валютi.";
+                case 4:
+                    return "Наш банк славиться гарною фiнансовою стабiльнiстю та захищенiстю даних";
+                default:
+                    return "Помилка! спробуйте ще раз";
+            }
+        }
+    }
+
+  
+    public class BankConsProxy : BankConsult
+    {
+        private BankCons bc;
+
+        public BankConsProxy()
+        {
+            bc = new BankCons();
+        }
+
+        public string GetCons(int q)
+        {
+            string ans;
+            if (q!=5)
+            {
+                ans = bc.GetCons(q);
+                return ans;
+            }
+            else
+                return "Вихiд";
+        }
+    }
+}
---
+++ Proxy/Proxy/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankConsult c = new BankConsProxy();
+            int num;
+
+            do
+            {
+                Console.WriteLine();
+                Console.WriteLine("1 - Якi документи потрiбнi для вiдкриття рахунку?");
+                Console.WriteLine("2 - Як можна поповнити рахунок?");
+                Console.WriteLine("3 - Яка мiнiмальна сума, яку можна зняти з банкомату?");
+                Console.WriteLine("4 - Якi переваги у вашого банку?");
+                Console.WriteLine("5 - Вихiд");
+                Console.WriteLine();
+
+                Console.Write("Напишiть номер запитання: ");
+                num = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine();
+                Console.WriteLine(c.GetCons(num));
+            } 
+            while (num!=5);
+
+            Console.WriteLine();
+        }
+    }
+}
